2d46d53ae889800756ba269c6571a782e5edbfb4
Flipmaxx <flipmaxxglobal@gmail.com>
2025-06-30 16:44:50 +0530

    remove

--- app/Hero/Homepage.jsx
+++ app/Hero/Homepage.jsx
@@ -31,7 +31,7 @@ export default function Homepage(){
       <BentoGallery/>
        <Whychoose/>
        <DestinationSection/>
-       <Testimonials/>
+       {/* <Testimonials/> */}
        <TravelArticles/>
 
         
